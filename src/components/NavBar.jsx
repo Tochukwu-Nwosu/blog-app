628e5e5696c8078af6203e55dcd387e124e197eb
User-XYZ <nwosu4686@gmail.com>
2023-07-05 21:39:39 +0100

    modified mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,10 @@
 import { MdOutlineDarkMode } from 'react-icons/md'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { useState } from 'react'
 
 const NavBar = () => {
+  const [ menu, setMenu ] = useState(false)  
+    
   return (
     <nav className="max-w-[1024px] px-4 mx-auto border-b-2">
       <div className="w-full flex justify-between">
@@ -12,19 +15,26 @@ const NavBar = () => {
                 <li className="mx-2 p-2">New Blog</li>
             </ul>
             <MdOutlineDarkMode size={25} className='mx-2 cursor-pointer' />
-            <AiOutlineMenu size={25} className='sm:hidden mx-2 cursor-pointer' />
-        </div>
-        {/* mobile view */}
-        <div className='absolute top-0 left-0 w-full h-screen flex justify-center items-center backdrop-blur-md'>
-            <ul className='flex flex-col justify-center items-center text-gray-500'> 
-                <li className="p-2 text-lg">Home</li>
-                <li className="p-2 text-lg">New Blog</li>
-            </ul>
-            <AiOutlineClose 
+            <AiOutlineMenu 
                 size={25} 
-                className='absolute top-4 right-4 cursor-pointer'
+                className='sm:hidden mx-2 cursor-pointer' 
+                onClick={() => setMenu(true)} 
             />
         </div>
+        {/* mobile menu */}
+        { menu && (
+            <div className='absolute top-0 left-0 w-full h-screen flex justify-center items-center backdrop-blur-md'>
+                <ul className='flex flex-col justify-center items-center text-gray-500'> 
+                    <li className="p-2 text-lg">Home</li>
+                    <li className="p-2 text-lg">New Blog</li>
+                </ul>
+                <AiOutlineClose 
+                    size={25} 
+                    className='absolute top-4 right-4 cursor-pointer'
+                    onClick={() => setMenu(false)}
+                />
+            </div>
+        )}
       </div>
     </nav>
   )
