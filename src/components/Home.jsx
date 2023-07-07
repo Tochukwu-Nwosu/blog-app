cf17ee0c72fabaaac1ba2c1f41d2f736766ddc89
User-XYZ <nwosu4686@gmail.com>
2023-07-07 23:46:13 +0100

    modified Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react"
 
 const Home = () => {
+  const [ blogs, setBlogs ] = useState([
+    {
+        id: 1,
+        title: "What is Machine Learning?",
+        author: "Isak Gupta",
+    },
+    {
+        id: 2,
+        title: "IoT, the future?",
+        author: "Micheal Mitovic",
+    }
+  ])
+
   return (
-    <div>
-      <h2>My Blog</h2>
+    <div className="my-6">
+      {blogs.map(blog => (
+        <div key={blog.id} className="my-6 px-4 py-2 shadow-sm rounded-md hover:shadow-md ease-in-out duration-100" >
+            <h2 className="text-xl my-2 font-semibold text-blue-600">{blog.title}</h2>
+            <p className="py-2 text-gray-600 italic">Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   )
 }
