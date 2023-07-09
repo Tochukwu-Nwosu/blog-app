b709109be1fdb1a60c01eb5c665e8ea5817c8db8
User-XYZ <nwosu4686@gmail.com>
2023-07-09 18:09:17 +0100

    modified Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,14 +24,14 @@ const Home = () => {
   
   return (
     <div className="my-6">
-      <h1 className="text-xl sm:text-4xl xl:text-5xl my-2 font-semibold text-blue-600">My Blogs</h1>
+      <h1 className="text-xl sm:text-4xl 2xl:text-5xl my-2 font-semibold text-blue-600">My Blogs</h1>
       { loading && <div className="py-2 xl:py-3">Loading...</div> }
       { error && <div className="py-2 xl:py-3">{error}</div> }
       { blogs && (
         blogs.map(({ id, title, author }) => (
-            <div key={id} className="my-6 px-4 xl:px-5 py-2 xl:py-3 shadow-sm rounded-md hover:shadow-md ease-in-out duration-100" >
-                <h2 className="text-lg sm:text-xl xl:text-2xl py-1 font-semibold text-blue-600">{title}</h2>
-                <p className="xl:text-lg py-1 text-gray-600 italic">Written by {author.name}</p>
+            <div key={id} className="my-6 px-4 2xl:px-5 py-2 2xl:py-3 shadow-sm rounded-md hover:shadow-md ease-in-out duration-100" >
+                <h2 className="text-lg sm:text-xl 2xl:text-2xl py-1 font-semibold text-blue-600">{title}</h2>
+                <p className="2xl:text-lg py-1 text-gray-600 italic">Written by {author.name}</p>
             </div>
         ))
       )}
