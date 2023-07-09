485a48c40c317a113cd5dfa051b5c1d1224adc37
User-XYZ <nwosu4686@gmail.com>
2023-07-09 17:38:35 +0100

    handled GET & Error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,37 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
 const Home = () => {
-  const [ blogs, setBlogs ] = useState([
-    {
-        id: 1,
-        title: "What is Machine Learning?",
-        author: "Isak Gupta",
-    },
-    {
-        id: 2,
-        title: "IoT, the future?",
-        author: "Micheal Mitrovic",
-    }
-  ])
+  const [ blogs, setBlogs ] = useState([])
+  const [ error, setError ] = useState('')
 
+  useEffect(() => {
+    axios.get('http://localhost:8000/blogs')
+        .then( response => {
+            setBlogs(response.data) 
+            setError('')
+        })
+        .catch( err => {
+            err.message === "Network Error" ? 
+                setError("Network Error!") : 
+                setError('Data Not Found!')
+            setBlogs([])
+        })
+  }, [])
+  
   return (
     <div className="my-6">
       <h1 className="text-xl sm:text-4xl xl:text-5xl my-2 font-semibold text-blue-600">My Blogs</h1>
-      {blogs.map(blog => (
-        <div key={blog.id} className="my-6 px-4 xl:px-5 py-2 xl:py-3 shadow-sm rounded-md hover:shadow-md ease-in-out duration-100" >
-            <h2 className="text-lg sm:text-xl xl:text-2xl py-1 font-semibold text-blue-600">{blog.title}</h2>
-            <p className="xl:text-lg py-1 text-gray-600 italic">Written by {blog.author}</p>
-        </div>
-      ))}
+      { error && <div className="py-2 xl:py-3">{error}</div> }
+      { blogs && (
+        blogs.map(({ id, title, author }) => (
+            <div key={id} className="my-6 px-4 xl:px-5 py-2 xl:py-3 shadow-sm rounded-md hover:shadow-md ease-in-out duration-100" >
+                <h2 className="text-lg sm:text-xl xl:text-2xl py-1 font-semibold text-blue-600">{title}</h2>
+                <p className="xl:text-lg py-1 text-gray-600 italic">Written by {author.name}</p>
+            </div>
+        ))
+      )}
+      
     </div>
   )
 }
