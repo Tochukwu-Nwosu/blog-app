69d4424ee5fe574fdfd266b030b358c229d715d6
User-XYZ <nwosu4686@gmail.com>
2023-07-09 15:04:33 +0100

    modified Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,9 +18,9 @@ const Home = () => {
     <div className="my-6">
       <h1 className="text-xl sm:text-4xl xl:text-5xl my-2 font-semibold text-blue-600">My Blogs</h1>
       {blogs.map(blog => (
-        <div key={blog.id} className="my-6 px-4 py-2 shadow-sm rounded-md hover:shadow-md ease-in-out duration-100" >
-            <h2 className="text-lg xl:text-2xl py-1 sm:py-2 font-semibold text-blue-600">{blog.title}</h2>
-            <p className="xl:text-lg py-1 sm:py-2 text-gray-600 italic">Written by {blog.author}</p>
+        <div key={blog.id} className="my-6 px-4 xl:px-5 py-2 xl:py-3 shadow-sm rounded-md hover:shadow-md ease-in-out duration-100" >
+            <h2 className="text-lg sm:text-xl xl:text-2xl py-1 font-semibold text-blue-600">{blog.title}</h2>
+            <p className="xl:text-lg py-1 text-gray-600 italic">Written by {blog.author}</p>
         </div>
       ))}
     </div>
