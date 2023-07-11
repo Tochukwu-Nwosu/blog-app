0426cab0511b00a609d294d8c10ff92f3e5040f9
User-XYZ <nwosu4686@gmail.com>
2023-07-11 02:26:04 +0100

    modified Blogs component

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -5,22 +5,21 @@ const Blogs = () => {
     const param = useParams()
     const { data: blog, error, loading } = useFetch(`http://localhost:8000/blogs/${param.id}`)
     const { title, date, author, image, body } = blog
-    console.log(blog)
 
     return (
-        <article className="my-6">
+        <article className="my-6 px-4 2xl:px-5 py-2 2xl:py-3">
             { loading && <div className="py-2 xl:py-3">Loading...</div> }
             { error && <div className="py-2 xl:py-3">{error}</div> }
             { blog && (
                 <>
-                    <h1 className="text-xl">{title}</h1>
-                    <div>
-                        <img src={author.imgSrc} alt="author's image" />
-                        <p>Written by {author.name} {date}</p>
+                    <h1 className="text-xl sm:text-4xl 2xl:text-5xl my-4 font-semibold text-blue-600">{title}</h1>
+                    <div className="w-full mt-6 mb-4 flex items-center gap-2">
+                        <img src={author.imgSrc} alt="author's image" className="w-[50px] h-[50px] object-cover rounded-full" />
+                        <p className="text-gray-600 italic">Written by {author.name} {date}</p>
                     </div>
-                    <div>
-                        <img src={image} alt="image" />
-                        <p>{body}</p>
+                    <div className="w-full my-6">
+                        <img src={image} alt="image" className="w-full h-[400px] object-cover mb-6" />
+                        <p className="text-justify">{body}</p>
                     </div>
                 </>
             ) }
