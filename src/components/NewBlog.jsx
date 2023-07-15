eba35864e363fede51c64f0c4057b7ac25d03c95
User-XYZ <nwosu4686@gmail.com>
2023-07-15 16:45:54 +0100

    modified form

--- src/components/NewBlog.jsx
+++ src/components/NewBlog.jsx
@@ -38,7 +38,6 @@ const NewBlog = () => {
 
   const { title, author: { imgSrc, name }, image, body } = data
   
-  // style form
   return (
     <div>
       <h1 className="text-3xl sm:text-4xl 2xl:text-5xl my-4 font-semibold text-blue-600">Add Blog</h1>
