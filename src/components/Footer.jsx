f8228b2562d3e31a30e0e43aa2d7c0f63c83d87e
User-XYZ <nwosu4686@gmail.com>
2023-07-27 16:40:54 +0100

    modified Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,10 @@ const Footer = () => {
   let year = moment().format('YYYY')
 
   return (
-    <footer className="max-w-[1024px] py-4 text-center mx-auto mt-8 border-t-2 text-gray-600 dark:border-gray-800 dark:text-gray-400">
-      <p>Copyright &copy; {year} TK Blog</p>
+    <footer className="w-full h-[58px] absolute bottom-0 inset-x-0 top-full bg-white dark:bg-slate-950">
+        <div className="max-w-[1024px] mx-auto py-4 text-center border-t-2 text-gray-600 dark:border-gray-800 dark:text-gray-400">
+            <p>Copyright &copy; {year} TK Blog</p>
+        </div>
     </footer>
   )
 }
