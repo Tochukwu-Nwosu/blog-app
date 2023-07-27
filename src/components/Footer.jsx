705dfcbc343c6e5b4a11db4865b69933512cbc65
User-XYZ <nwosu4686@gmail.com>
2023-07-27 16:57:50 +0100

    modified Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer className="w-full h-[58px] absolute bottom-0 inset-x-0 top-full bg-white dark:bg-slate-950">
-        <div className="max-w-[1024px] mx-auto py-4 text-center border-t-2 text-gray-600 dark:border-gray-800 dark:text-gray-400">
+        <div className="max-w-[1024px] mx-auto py-4 text-center border-t-2 text-sm text-gray-600 dark:border-gray-800 dark:text-gray-400">
             <p>Copyright &copy; {year} TK Blog</p>
         </div>
     </footer>
