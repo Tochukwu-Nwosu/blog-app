e5bae32de282d14a385203af98d62fa1d85b9eaf
User-XYZ <nwosu4686@gmail.com>
2023-09-22 18:19:59 +0100

    created useTheme hook

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,17 @@ import Blogs from "./components/Blogs"
 import Home from "./components/Home"
 import NavBar from "./components/NavBar"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useState } from "react"
+import useTheme from "./components/useTheme"
 
 
 const App = () => {
   
-  const [ theme, setTheme ] = useState(() => {
-    /* This function assigns the value of 'theme' in the localstorage to the theme variable if 'theme' key exist, 
-       if not it assigns 'light' to the theme variable.
-       This makes the theme persistent even when refreshed.
-    */
-    let initialTheme = localStorage.getItem('theme')
-    initialTheme = initialTheme ? initialTheme : 'light'
-    return initialTheme
-  })
-  
-  const toggleTheme = () => {
-    /* This function sets the localstorage 'theme' to the opposite value of the current theme variable and updates the theme variable.  */
-    setTheme((prevTheme) => {
-      let newTheme = prevTheme === 'light' ? 'dark' : 'light'
-      localStorage.setItem('theme', newTheme)
-      return newTheme
-    })  
-  }
-  
+  const [ theme, toggleTheme ] = useTheme()
   console.log(theme)
-  // console.log(document.documentElement)
 
   return (
     <Router>
-      <div className={ `w-full min-h-screen relative ${ theme === 'dark' ? ` dark bg-slate-950` : ` bg-white` }` }>
+      <div className="w-full min-h-screen relative dark:bg-slate-950 bg-white">
         <NavBar theme={theme} toggleTheme={toggleTheme} />
         <main className="max-w-[1024px] px-4 py-8 mx-auto">
           <Routes>
@@ -51,3 +32,4 @@ const App = () => {
 }
 
 export default App
+{/* <div className={ `w-full min-h-screen relative ${ theme === 'dark' ? ` dark bg-slate-950` : ` bg-white` }` }> */}
